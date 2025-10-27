53cd013b4ab44d02fd932fed463fdc3f94a119e8
Edu-Carrizo <eduardo.andres.carrizo@gmail.com>
2025-10-26 23:44:12 -0300

    segundo commit archivos basicos de pre-entrega, con errores

--- ttcarrizoreactweb/src/App.jsx
+++ ttcarrizoreactweb/src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Inicio from './pages/Inicio'
+import Servicios from './pages/Servicios'
+import Navbar from './pages/Navbar'
+import Productos from './pages/Productos'
+import ProductoDetalle from './pages/DetalleProdutos'
+import Pagar from "./pages/Pagar";
+import RutaProtegida from "./pages/RutaProtegida";
+import IniciarSesion from "./pages/IniciarSesion";
+import Footer from './pages/Footer'
+import { Routes, Route } from 'react-router-dom'
+import { AppProvider } from './context/AppContext'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <AppProvider>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Inicio />} />
+        <Route path='/servicios' element={<Servicios />} />
+        <Route path='/productos' element={<Productos />} />
+        <Route path='/productos/:id' element={<ProductoDetalle />} />
+        <Route path='/productos/:categoria/:id' element={<ProductoDetalle />} />
+        <Route path="/iniciar-sesion" element={<IniciarSesion />
+          }
+        />
+        <Route path="/pagar" element={ <RutaProtegida>
+              <Pagar  />
+            </RutaProtegida>
+          }
+        />
+      </Routes>
+      <Footer />
+    </div>
+    </AppProvider>
   )
 }
 
